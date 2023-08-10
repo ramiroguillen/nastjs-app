2a2566c7f9364f82bca95515f478771e7f136701
ramiroguillen <ramirojguillen@gmail.com>
2023-08-10 18:43:07 -0300

    initials modules

---
+++ src/products/products.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+import { ProductEntity } from 'src/database/entities/product.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ProductEntity])],
+  controllers: [ProductsController],
+  providers: [ProductsService],
+})
+export class ProductsModule {}
