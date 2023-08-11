d5c18118ae165320943c0f897da4d304ba658300
ramiroguillen <ramirojguillen@gmail.com>
2023-08-11 17:41:03 -0300

    jwt strategy and get current user route

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,12 +3,36 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 
+import { UserRoles } from 'src/user/types/userRoles.type';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/database/entities/user.entity';
+import { Repository } from 'typeorm';
+
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    config: ConfigService,
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+
+  /**
+   * validate
+   */
+  public async validate(payload: {
+    sub: number;
+    email: string;
+    role: UserRoles;
+  }) {
+    const user = await this.userRepository.findOneBy({
+      id: payload.sub.toString(),
+    });
+    delete user.password;
+    return user;
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+
+import { UserService } from './users.service';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
+  @UseGuards(JwtGuard)
   @Get('me')
-  getCurrentUser() {}
+  /**
+   * getCurrentUser
+   */
+  public async getCurrentUser(@Req() req: Request) {
+    return req.user;
+  }
 }
